ae6a35d31919ab56e885f85b54ea576a0627a1db
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-02-18 11:53:39 +0000

    Checking the queue gives error and I don't know why

--- src/write_PBS.py
+++ src/write_PBS.py
@@ -1,17 +1,31 @@
 def write_PBSheader(job_number, json):
+    import sys
     """
     Writes the header with the PBS information
     """
     job_str = str(job_number).zfill(2)
-    file = open("%s_job%s.sh" % (json['simulation_details']['system_name'],
+    file = open("%s-job%s.sh" % (json['simulation_details']['system_name'],
                                  job_str), "w")
     file.write("#PBS -N %s_job%s\n" % (json['simulation_details']['system_name'],
                                        job_str))
-    file.write("#PBS -l select=%s\n" % json['pbs_settings']['nnodes'])
-    file.write("#PBS -l ncpus=%s:ngpus=%s\n" % (json['pbs_settings']['ncpus'],
-                                                json['pbs_settings']['ngpus']))
-    file.write("#PBS -l walltime=%s\n" % json['pbs_settings']['walltime'])
-    file.write("#PBS -l host=%s\n" % json['pbs_settings']['host'])
+
+    if ['pbs_settings']['queue'] == 'gpgpu':
+        file.write("""PBS -lselect=%s:ncpus=%s:ngpus=%s:mem=%s:gpu_type=%s\n
+                   """ % (json['pbs_settings']['nnodes'],
+                          json['pbs_settings']['ncpus'],
+                          json['pbs_settings']['ngpus'],
+                          json['pbs_settings']['mem'],
+                          json['pbs_settings']['gpu_type']))
+    elif ['pbs_settings']['queue'] == 'pqigould':
+        file.write("""PBS -lselect=%s:ncpus=%s:ngpus=%s:mem=%s:host=%s\n
+                   """ % (json['pbs_settings']['nnodes'],
+                          json['pbs_settings']['ncpus'],
+                          json['pbs_settings']['ngpus'],
+                          json['pbs_settings']['mem'],
+                          json['pbs_settings']['host']))
+    else:
+        sys.exit("Supported queues are 'pgigould' or 'gpgpu' only.")
+
     file.write("#PBS -q %s\n" % json['pbs_settings']['queue'])
     file.write("#PBS -M %s\n" % json['pbs_settings']['email'])
     file.write("#PBS -m abe\n\n")
