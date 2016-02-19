03d44f69216994e0fc6aa9c9249273b66c1980ff
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-02-19 10:08:08 +0000

    New CUDA version and binary location on the HPC

--- src/write_MDcmds.py
+++ src/write_MDcmds.py
@@ -3,7 +3,8 @@ def write_MDscript(job_number, json, timeList):
     with open("%s_job%s.pbs" % (json['simulation_details']['system_name'],
                                 job_str), "a") as file:
 
-        file.write("module load cuda/6.5.19\n\n")
+        file.write("module load cuda/7.5.18\n")  # CUDA version
+        file.write("module load intel-suite\n\n")
         file.write("prmtop=%s\n" % json['simulation_details']['topology_file'])
         file.write("sim=%s\n\n" % timeList[job_number])
 
@@ -13,11 +14,11 @@ def write_MDscript(job_number, json, timeList):
             file.write("cp %s/*.in .\n" % json['simulation_details']['job_directory'])
             file.write("cp %s/${inpcrd} .\n" % json['simulation_details']['job_directory'])
             file.write("cp %s/${prmtop} .\n\n" % json['simulation_details']['job_directory'])
-            file.write("/home/igould/pmemd.cuda_SPFP -O -i premin.in -o premin.out -c ${inpcrd} -p ${prmtop} -r premin.rst -ref ${inpcrd}\n\n")
-            file.write("/home/igould/pmemd.cuda_SPFP -O -i sandermin1.in -o sandermin1.out -c premin.rst -p ${prmtop} -r sandermin1.rst\n\n")
-            file.write("/home/igould/pmemd.cuda_SPFP -O -i 02_Heat.in -o 02_Heat.out -c sandermin1.rst -p ${prmtop} -r 02_Heat.rst -ref sandermin1.rst -x 02_Heat.nc\n\n")
-            file.write("/home/igould/pmemd.cuda_SPFP -O -i 03_Heat2.in -o 03_Heat2.out -c 02_Heat.rst -p ${prmtop} -r 03_Heat2.rst -ref 02_Heat.rst -x 03_Heat2.nc\n\n")
-            file.write("pbsexec -grace 15 /home/igould/pmemd.cuda_SPFP -O -i 05_Prod.in -o %s_${sim}ns.out -c 03_Heat2.rst -p ${prmtop} -r %s_${sim}ns.rst -x %s_${sim}ns.nc\n\n"
+            file.write("/home/igould/newamber/amber/bin/pmemd.cuda_SPFP -O -i premin.in -o premin.out -c ${inpcrd} -p ${prmtop} -r premin.rst -ref ${inpcrd}\n\n")
+            file.write("/home/igould/newamber/amber/bin/pmemd.cuda_SPFP -O -i sandermin1.in -o sandermin1.out -c premin.rst -p ${prmtop} -r sandermin1.rst\n\n")
+            file.write("/home/igould/newamber/amber/bin/pmemd.cuda_SPFP -O -i 02_Heat.in -o 02_Heat.out -c sandermin1.rst -p ${prmtop} -r 02_Heat.rst -ref sandermin1.rst -x 02_Heat.nc\n\n")
+            file.write("/home/igould/newamber/amber/bin/pmemd.cuda_SPFP -O -i 03_Heat2.in -o 03_Heat2.out -c 02_Heat.rst -p ${prmtop} -r 03_Heat2.rst -ref 02_Heat.rst -x 03_Heat2.nc\n\n")
+            file.write("pbsexec -grace 15 /home/igould/newamber/amber/bin/pmemd.cuda_SPFP -O -i 05_Prod.in -o %s_${sim}ns.out -c 03_Heat2.rst -p ${prmtop} -r %s_${sim}ns.rst -x 05_Prod_%s_${sim}ns.nc\n\n"
                        % (json['simulation_details']['system_name'],
                           json['simulation_details']['system_name'],
                           json['simulation_details']['system_name']))
@@ -34,7 +35,7 @@ def write_MDscript(job_number, json, timeList):
             file.write("cp %s/05_Prod.in .\n" % json['simulation_details']['job_directory'])
             file.write("cp %s/${prevrst} .\n" % json['simulation_details']['job_directory'])
             file.write("cp %s/${prmtop} .\n\n" % json['simulation_details']['job_directory'])
-            file.write("pbsexec -grace 15 /home/igould/pmemd.cuda_SPFP -O -i 05_Prod.in -o %s_${sim}ns.out -c ${prevrst} -p ${prmtop} -r %s_${sim}ns.rst -x %s_${sim}ns.nc\n\n"
+            file.write("pbsexec -grace 15 /home/igould/newamber/amber/bin/pmemd.cuda_SPFP -O -i 05_Prod.in -o %s_${sim}ns.out -c ${prevrst} -p ${prmtop} -r %s_${sim}ns.rst -x 05_Prod_%s_${sim}ns.nc\n\n"
                        % (json['simulation_details']['system_name'],
                           json['simulation_details']['system_name'],
                           json['simulation_details']['system_name']))
