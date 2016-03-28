849f1caf600fa5f5306f4504e99202e0e8cb1c44
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-03-28 19:33:42 +0200

    Added class variables from JSON file

--- src/Simulation.py
+++ src/Simulation.py
@@ -9,6 +9,22 @@ class Simulation:
         self.walltime = json['pbs_settings']['walltime']
         self.mail = json['pbs_settings']['email']
 
+        # Simulation details
+        self.system_name = json['simulation_details']['system_name']
+        self.inpcrd_file = json['simulation_details']['inpcrd_file']
+        self.topology_file = json['simulation_details']['topology_file']
+        self.start_time = json['simulation_details']['start_time']
+        self.final_time = json['simulation_details']['final_time']
+        self.job_length = json['simulation_details']['job_length']
+        self.job_directory = json['simulation_details']['job_directory']
+        self.start_rst = json['simulation_details']['start_rst']
+        self.pre_simulation_cmd = json['simulation_details']['pre_simulation_cmd']
+
+        # Local machine details
+        self.user = json['local_machine']['user']
+        self.hostname = json['local_machine']['hostname']
+        self.destination = json['local_machine']['destination']
+
     def generateSimulationFiles(self):
         self._generate_PBS_headers()
         self._generate_pre_simulation_file()
