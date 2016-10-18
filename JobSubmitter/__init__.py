af81f63a6c0bc4cfda2843923101d00619cf92ad
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-10-18 18:08:10 +0100

    non case sensitive filesystem made me go crazy

---
+++ JobSubmitter/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Juan Eiros"""
+__email__ = 'juaneiros@hotmail.com'
+__version__ = '0.1.0'
--- jobsubmitter/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """Juan Eiros"""
-__email__ = 'juaneiros@hotmail.com'
-__version__ = '0.1.0'
