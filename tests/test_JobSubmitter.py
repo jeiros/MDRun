449b4066a2c1a967e5830c960143335c6322fb6a
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-10-19 12:20:57 +0100

    Tests pass!

--- tests/test_JobSubmitter.py
+++ tests/test_JobSubmitter.py
@@ -13,38 +13,26 @@ import sys
 import unittest
 from contextlib import contextmanager
 from click.testing import CliRunner
-
 import JobSubmitter
+from JobSubmitter import cli
 
 
-# class TestJobsubmitter(unittest.TestCase):
-
-#     def setUp(self):
-#         pass
-
-#     def tearDown(self):
-#         pass
-
-#     def test_000_something(self):
-#         pass
-
-#     def test_command_line_interface(self):
-#         runner = CliRunner()
-#         result = runner.invoke(cli.main)
-#         assert result.exit_code == 0
-#         assert 'JobSubmitter.cli.main' in result.output
-#         help_result = runner.invoke(cli.main, ['--help'])
-#         assert help_result.exit_code == 0
-#         assert '--help  Show this message and exit.' in help_result.output
-
-
-class TestHelloWorld(unittest.TestCase):
-
-    """docstring for TestHelloWorld"""
+class TestJobsubmitter(unittest.TestCase):
 
     def setUp(self):
-        self.hello_message = "Hello, world"
+        pass
 
-    def test_print_hello_world(self):
-        output = JobSubmitter.cli.hello()
-        assert(output == self.hello_message)
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+    def test_command_line_interface(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.main)
+        assert result.exit_code == 0
+        assert 'JobSubmitter.cli.main' in result.output
+        help_result = runner.invoke(cli.main, ['--help'])
+        assert help_result.exit_code == 0
+        assert '--help  Show this message and exit.' in help_result.output
